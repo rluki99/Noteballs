9775e8cd611afdc7c9bc1ee4d0373b2bad072c4c
rluki99 <lukasz.beczek24@gmail.com>
2023-12-28 13:14:25 +0100

    feat: delete note modal

---
+++ src/components/Notes/ModalDeleteNote.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="modal is-active p-2">
+		<div class="modal-background"></div>
+		<div ref="modalCardRef" class="modal-card">
+			<header class="modal-card-head">
+				<p class="modal-card-title">Delete Note?</p>
+				<button @click="closeModal" class="delete" aria-label="close"></button>
+			</header>
+			<section class="modal-card-body">
+				Are you sure you want to delete this note?
+			</section>
+			<footer class="modal-card-foot is-justify-content-flex-end">
+				<button @click="closeModal" class="button">Cancel</button>
+				<button @click="storeNotes.deleteNote(noteId)" class="button is-danger">Delete</button>
+			</footer>
+		</div>
+	</div>
+</template>
+
+<script setup>
+// imports
+
+import { ref, onMounted, onUnmounted } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import { useStoreNotes } from '@/stores/storeNotes'
+
+// props
+
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        default: false
+    },
+    noteId: {
+        type: String,
+        required: true
+    }
+})
+
+// emits
+
+const emit = defineEmits(['update:modelValue'])
+
+// store
+
+const storeNotes = useStoreNotes()
+
+// close modal
+
+const closeModal = () => {
+    emit('update:modelValue', false)
+}
+
+// click outside to close
+
+const modalCardRef = ref(null)
+
+onClickOutside(modalCardRef, closeModal)
+
+// keyboard control
+
+const handleKeyboard = (e) => {
+    if(e.key === 'Escape') closeModal()
+}
+
+onMounted(() => {
+    document.addEventListener('keyup', handleKeyboard)
+})
+
+onUnmounted(() => {
+    document.removeEventListener('keyup', handleKeyboard)
+})
+
+</script>
--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -10,16 +10,18 @@
 		</div>
 		<footer class="card-footer">
 			<RouterLink :to="`/editNote/${note.id}`" href="#" class="card-footer-item">Edit</RouterLink>
-			<a @click.prevent="storeNotes.deleteNote(note.id)" href="#" class="card-footer-item">Delete</a>
+			<a @click.prevent="modals.deleteNote = true" href="#" class="card-footer-item">Delete</a>
 		</footer>
+		<ModalDeleteNote v-model="modals.deleteNote" v-if="modals.deleteNote" :noteId="note.id"/>
 	</div>
 </template>
 
 <script setup>
 // imports
 
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
 import { useStoreNotes } from '@/stores/storeNotes'
+import ModalDeleteNote from '@/components/Notes/ModalDeleteNote.vue'
 
 // props
 
@@ -40,6 +42,12 @@ const characterLength = computed(()=> {
     const length = props.note.content.length
     const description = length > 1 ? 'characters' : 'character'
     return `${length} ${description}`
+})
+
+// modals
+
+const modals = reactive({
+	deleteNote: false
 })
 
 </script>
