554618074c9f0e5dd8a18011d0680d06509f3003
rluki99 <lukasz.beczek24@gmail.com>
2023-12-29 13:40:44 +0100

    feat: Firebase init

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCvYKeyZViZMA8F_MK73tXSjN6BC8i4Ffw',
+	authDomain: 'noteballs-69f9d.firebaseapp.com',
+	projectId: 'noteballs-69f9d',
+	storageBucket: 'noteballs-69f9d.appspot.com',
+	messagingSenderId: '116826402323',
+	appId: '1:116826402323:web:650030d81f6d7cdef859e6',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app);
+
+export {
+    db
+}
