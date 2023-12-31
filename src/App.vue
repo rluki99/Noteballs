c441034e72c2be097f5d8bd9da164675505a891e
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-12-31 11:43:36 +0100

    feat: display notes from firestorm db

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,19 @@
 // imports
 
 import NavBar from '@/components/layout/NavBar.vue'
+import { useStoreNotes } from '@/stores/storeNotes'
+import { onMounted } from 'vue';
+
+// store
+
+const storeNotes = useStoreNotes()
+
+// mounted
+
+onMounted(() => {
+	storeNotes.getNotes()
+})
+
 </script>
 
 <style>
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,23 +1,34 @@
 import { defineStore } from 'pinia'
+import { collection, getDocs } from "firebase/firestore";
 import { db } from '@/js/firebase'
 
 export const useStoreNotes = defineStore('storeNotes', {
 	state: () => {
 		return {
 			notes: [
-				{
-					id: 'id1',
-					content:
-						'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolor dicta mollitia architecto quo quod ipsam sit itaque quas quam dolorem fuga praesentium suscipit possimus quidem et ipsa nulla reiciendis.',
-				},
-				{
-					id: 'id2',
-					content: 'Shorter note. Lorem ipsum dolar sit',
-				},
+				// {
+				// 	id: 'id1',
+				// 	content:
+				// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolor dicta mollitia architecto quo quod ipsam sit itaque quas quam dolorem fuga praesentium suscipit possimus quidem et ipsa nulla reiciendis.',
+				// },
+				// {
+				// 	id: 'id2',
+				// 	content: 'Shorter note. Lorem ipsum dolar sit',
+				// },
 			],
 		}
 	},
 	actions: {
+		async getNotes() {
+			const querySnapshot = await getDocs(collection(db, "notes"));
+			querySnapshot.forEach((doc) => {
+				let note = {
+					id: doc.id,
+					content: doc.data().content
+				}
+				this.notes.push(note)
+			});
+		},
 		addNote(newNoteContent) {
 			const currentDate = new Date().getTime()
 			const id = currentDate.toString()
